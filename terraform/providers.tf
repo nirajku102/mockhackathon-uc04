db50028dd0fdd95cad0f77582b41b269a62ecd5c
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-04-18 08:29:43 +0000

    integrating checkov and tflint

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  required_version = ">= 1.0.0" # Specify the minimum Terraform version
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0" # Specify the AWS provider version
+    }
+  }
+
   backend "s3" {
     bucket         = "mockhackathon-uc04"
     key            = "terraform"
